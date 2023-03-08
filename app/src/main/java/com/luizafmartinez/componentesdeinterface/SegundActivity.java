e4b6eb44fb6b8c93a300e50aa0d85f9b6277a9e0
alxmart <alxmartinez@gmail.com>
2023-03-08 20:27:33 -0300

    Layout tests.

---
+++ app/src/main/java/com/luizafmartinez/componentesdeinterface/SegundActivity.java
@@ -0,0 +1,14 @@
+package com.luizafmartinez.componentesdeinterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SegundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segund);
+    }
+}
