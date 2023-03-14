f1a2745068f7fa6d98ad8a48e62f82378f51e97a
alxmart <alxmartinez@gmail.com>
2023-03-14 13:17:52 -0300

    Layout tests.

--- app/src/main/java/com/luizafmartinez/componentesdeinterface/CaixaActivity.java
+++ app/src/main/java/com/luizafmartinez/componentesdeinterface/CaixaActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class CaixaActivity extends AppCompatActivity {
 
     @Override
@@ -14,10 +16,12 @@ public class CaixaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_caixa);
 
         Button botao = findViewById(R.id.botao_salvar);
+        TextInputEditText email = findViewById( R.id.edit_mail );
 
-        botao.setOnClickListener(view ->
-                System.out.println("Botão foi clicado")
-        );
+        botao.setOnClickListener(view -> {
+            String emailUsuario = email.getText().toString();
+            System.out.println("Email selecionado:" + emailUsuario);
+        });
     }
 
     /*public void executar( View view ) {
