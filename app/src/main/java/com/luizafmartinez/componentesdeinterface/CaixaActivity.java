8d0382e76fd5034769eb0303a9d3f6482b201367
alxmart <alxmartinez@gmail.com>
2023-03-14 10:39:56 -0300

    Layout tests.

--- app/src/main/java/com/luizafmartinez/componentesdeinterface/CaixaActivity.java
+++ app/src/main/java/com/luizafmartinez/componentesdeinterface/CaixaActivity.java
@@ -3,6 +3,8 @@ package com.luizafmartinez.componentesdeinterface;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CaixaActivity extends AppCompatActivity {
 
@@ -10,5 +12,28 @@ public class CaixaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caixa);
+
+        Button botao = findViewById(R.id.botao_salvar);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Botão foi clicado");
+            }
+        });
+
+
+
+
     }
+
+    /*public void executar( View view ) {
+
+    }*/
+
+
+
+
+
+
 }
